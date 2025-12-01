5f7dc106008a9da6f99b88eb4ec0b63f88781345
Jeff Fessler <fessler@umich.edu>
2025-11-30 22:40:00 -0500

    bspline demo

--- docs/lit/examples/01-spline.jl
+++ docs/lit/examples/01-spline.jl
@@ -25,10 +25,14 @@ This page was generated from a single Julia file:
 
 # Packages needed here.
 
-using Template
+using BSplineKit
+using LinearAlgebra: I, norm
 using MIRTjim: jim, prompt
 using InteractiveUtils: versioninfo
-using Pltos
+using Plots
+using Random: seed!
+default(markerstrokecolor=:auto, label="")
+seed!(0)
 
 
 # The following line is helpful when running this file as a script;
@@ -37,19 +41,70 @@ using Pltos
 isinteractive() ? jim(:prompt, true) : prompt(:draw);
 
 
-# ### Setup
+# ### Data
+
+N = 100
+tmax = 3
+t = tmax * sort(rand(N)) # random samples on [0,3]
+f(t) = 1 < t < 2 # rect function
+y = f.(t); # noiseless data for now
+
+# plot data and true signal
+yaxis = ((-0.2, 1.4), -0.2:0.2:1.4)
+p0 = scatter(t, y; yaxis, color=:black, label="data")
+plot!(p0, tf, f.(tf), label="true")
+
+
+# B-spline settings
+degree = 3
+M = 40
+knots = range(0, tmax, M) |> collect
+
+# Make B-spline basis objects
+basis = BSplineBasis(BSplineOrder(degree + 1), knots) # ; periodic=true
+
+# Plot finely sampled B-spline basis functions
+tf = range(-0.3, tmax+0.3, 1001)
+Bf = hcat([b.(tf) for b in basis]...)
+pb1 = plot(tf, Bf; title="Basis functions, degree=$degree")
+
+# Sample B-spline basis objects at measurement points
+B = hcat([b.(t) for b in basis]...)
+pb2 = deepcopy(pb1)
+scatter!(pb2, t, B; title="Basis function samples")
+
+# LS fit of B-splines to data
+xh = B \ y
+yf = +([b.(tf) * xh[i] for (i,b) in enumerate(basis)]...)
+
+p1 = deepcopy(p0)
+plot!(p1, tf, yf; label="bspline")
+
 
 #=
+Evidently the unregularized B-spline fit here
+is over-fitting, even though ``N < M``,
+so next we try simple Tikhonov regularization.
+=#
+β = 2e-1
+A = [B; sqrt(β)*I]
+
+yz = [y; zeros(length(basis))]
+xr = A \ yz
+yfr = +([b.(tf) * xr[i] for (i,b) in enumerate(basis)]...)
+
+p2 = deepcopy(p0)
+plot!(p2, tf, yfr, label="bspline β=$β")
+
+
+#=
+But what regularization parameter β to choose?
+
+Cross-validation is one approach to choose.
+
+todo
 =#
 
 
-# ### Reproducibility
 
-# This page was generated with the following version of Julia:
-
-io = IOBuffer(); versioninfo(io); split(String(take!(io)), '\n')
-
-
-# And with the following package versions
-
-import Pkg; Pkg.status()
+#todo include("../../../inc/reproduce.jl")
