664e1d9067e4005e9221395a4cb5c05184d73c02
Jeff Fessler <fessler@umich.edu>
2025-12-08 20:25:53 -0500

    Fix NRMSE calculation

--- docs/lit/examples/01-spline.jl
+++ docs/lit/examples/01-spline.jl
@@ -51,9 +51,10 @@ y = f.(t); # noiseless data for now
 
 # plot data and true signal (finely sampled)
 yaxis = ((-0.2, 1.4), -0.2:0.2:1.4)
-tf = range(-0.3, tmax+0.3, 1001)
+tf = range(-0.3, tmax+0.3, 1001) # fine grid
+ytf = f.(tf) # true function on a fine grid
 p0 = scatter(t, y; yaxis, color=:black, label="data")
-plot!(p0, tf, f.(tf), label="true")
+plot!(p0, tf, ytf, label="true")
 
 
 # B-spline settings
@@ -75,10 +76,14 @@ scatter!(pb2, t, B; title="Basis function samples")
 
 # LS fit of B-splines to data
 xh = B \ y
-yf = +([b.(tf) * xh[i] for (i,b) in enumerate(basis)]...)
+yhf = +([b.(tf) * xh[i] for (i,b) in enumerate(basis)]...);
+
+# Utility functions for NRMSE
+do_nrmse(yr::AbstractVector) = norm(yr - ytf) / norm(ytf)
+round3(x) = round(x, digits=3);
 
 p1 = deepcopy(p0)
-plot!(p1, tf, yf; label="bspline")
+plot!(p1, tf, yhf; label="bspline, NRMSE=$(round3(do_nrmse(yhf)))")
 
 
 #=
@@ -86,15 +91,20 @@ Evidently the unregularized B-spline fit here
 is over-fitting, even though ``N < M``,
 so next we try simple Tikhonov regularization.
 =#
-β = 2e-1
-A = [B; sqrt(β)*I]
+function do_fit(β::Real)
+    A = [B; sqrt(β)*I]
+    yz = [y; zeros(length(basis))]
+    xr = A \ yz
+    yr = +([b.(tf) * xr[i] for (i,b) in enumerate(basis)]...)
+end
+nrmse_fit(β::Real) = do_nrmse(do_fit(β));
 
-yz = [y; zeros(length(basis))]
-xr = A \ yz
-yfr = +([b.(tf) * xr[i] for (i,b) in enumerate(basis)]...)
+β = 2e-1
+yfr = do_fit(β);
 
 p2 = deepcopy(p0)
-plot!(p2, tf, yfr, label="bspline β=$β")
+plot!(p2, tf, yfr,
+ label = "bspline β=$(round3(β)), NRMSE=$(round3(do_nrmse(yfr)))")
 
 
 #=
@@ -109,17 +119,28 @@ to the best fit to the true function
 
 =#
 
-function nrmse_fit(β::Real)
-    A = [B; sqrt(β)*I]
-    xr = A \ yz
-    yr = +([b.(tf) * xr[i] for (i,b) in enumerate(basis)]...)
-    return norm(yr - yf) / norm(yf)
-end;
+#=
+It turns out here that Tikhonov regularization
+only slightly reduces NRMSE,
+even in this very favorable case
+where we use the ground-truth "oracle" to select β.
 
-# It turns out here that Tikhonov regularization does not reduce NRMSE:
-βlist = 10 .^ (-5:0.2:5)
+Apparently Tikhonov regularization is ineffective
+for (uniformly spaced) B-spline fitting to non-uniformly data.
+=#
+βlist = 10 .^ (-5:0.2:1)
 nrmse = nrmse_fit.(βlist)
-pn = plot(log10.(βlist), nrmse; title="NRMSE", xlabel="log10(β)")
+βbest = argmin(nrmse_fit, βlist)
+pn = plot(log10.(βlist), 100*nrmse; title="NRMSE (%)", xlabel="log10(β)",
+ yaxis = ("NRMSE (%)", (10, 30)),
+)
+plot!(log10(βbest) * [1, 1], [10, 40], color=:red)
+
+yfb = do_fit(βbest)
+p3 = deepcopy(p0)
+nrmse_best = nrmse_fit(βbest)
+plot!(p3, tf, yfb,
+ label="bspline β=$(round3(βbest)), NRMSE=$(round3(nrmse_best))")
 
 
 include("../../inc/reproduce.jl")
